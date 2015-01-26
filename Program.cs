afd420a21b0ea5d4ef45cc5e1dd34d36fdb036e9
Fabian Wetzel <fabsenet@googlemail.com>
2015-01-26 23:01:54 +0100

    added nlog; added mirror download of all documents to ravenfs

--- Program.cs
+++ Program.cs
@@ -1,67 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NLog;
+using Raven.Abstractions.FileSystem;
 using Raven.Client.Document;
+using Raven.Client.FileSystem;
+using Raven.Json.Linq;
 
 namespace Fabsenet.EdiEnergy
 {
-    class Program
+    static class Program
     {
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
+        {
+            try
+            {
+                _log.Debug("EdiEnergyExtractor started.");
+                AsyncMain(args).Wait();
+            }
+            catch (Exception ex)
+            {
+                _log.Fatal("Unhandled exception! {0}", ex.ToString());
+            }
+        }
+
+        private static async Task AsyncMain(string[] args)
         {
             var dataExtractor = new DataExtractor();
             if (args.Any())
             {
+                _log.Warn("Using file as ressource '{0}'", args[0]);
                 dataExtractor.LoadFromFile(args[0]);
             }
             else
             {
                 //request data from actual web page
+                _log.Info("using web as actual ressource");
                 dataExtractor.LoadFromWeb();
             }
 
             dataExtractor.AnalyzeResult();
+            await StoreOrUpdateInRavenDb(dataExtractor.Documents);
 
-            var json = dataExtractor.GetResultAsJson();
-            StoreOrUpdateInRavenDb(dataExtractor.Documents);
-            Console.WriteLine(json);
-
-            const string edienergyJsonFile = "EdiEnergy.json";
-            WriteFileIfChanged(edienergyJsonFile, json);
-
-            Console.WriteLine("+++Done+++");
-            Console.ReadLine();
+            _log.Debug("Done");
         }
 
-        private static void StoreOrUpdateInRavenDb(List<EdiDocument> ediDocuments)
+        private static async Task StoreOrUpdateInRavenDb(List<EdiDocument> ediDocuments)
         {
+            _log.Debug("Saving " + ediDocuments.Count + " documents to ravendb");
+
             var database = new DocumentStore()
             {
                 ConnectionStringName = "RavenDB"
-            }.Initialize();
+            }.Initialize(true);
 
-            using (var session = database.OpenSession())
+            using (var session = database.OpenAsyncSession())
             {
                 foreach (var ediDocument in ediDocuments)
                 {
-                    session.Store(ediDocument);                    
+                    await DownloadMirror(ediDocument);
+                    await session.StoreAsync(ediDocument);
                 }
-                session.SaveChanges();
+                await session.SaveChangesAsync();
             }
         }
 
-        private static void WriteFileIfChanged(string edienergyJsonFile, string json)
+        private static async Task DownloadMirror(EdiDocument ediDocument)
         {
-            if (!File.Exists(edienergyJsonFile) || json != File.ReadAllText(edienergyJsonFile))
+            _log.Debug("testing mirror file availability for {0}", ediDocument.Id);
+            var filesystem = new FilesStore() {ConnectionStringName = "RavenFS"}.Initialize(true);
+            using (var session = filesystem.OpenAsyncSession())
             {
-                Console.WriteLine("Writing file: {0}", edienergyJsonFile);
-                File.WriteAllText(edienergyJsonFile, json);
-            }
-            else
-            {
-                Console.WriteLine("Skipping file write, because nothing changed! ({0})", edienergyJsonFile);
-                
+                var file = await session.Query()
+                    .WhereEquals("OriginalUri", ediDocument.DocumentUri.ToString())
+                    .FirstOrDefaultAsync();
+
+                _log.Debug(file == null ? "The file does not exist" : "the file is mirrored");
+
+                if (file == null)
+                {
+                    _log.Debug("Downloading copy of ressource '{0}'", ediDocument.DocumentUri);
+                    var client = new HttpClient();
+                    var responseMessage = await client.GetAsync(ediDocument.DocumentUri);
+                    responseMessage.EnsureSuccessStatusCode();
+
+                    session.RegisterUpload(new FileHeader(ediDocument.Id, new RavenJObject()
+                    {
+                        {"OriginalUri", new RavenJValue(ediDocument.DocumentUri.ToString())}
+                    }), await responseMessage.Content.ReadAsStreamAsync());
+
+                    await session.SaveChangesAsync();
+                    _log.Debug("Stored copy of ressource '{0}'", ediDocument.DocumentUri);
+                }
             }
         }
     }
